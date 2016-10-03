81085a34d497fbefab373f88a9260a109ea1140d
nastyafilip <nastyafilip@mail.ru>
2016-10-03 22:33:23 +0600

    релиз 2.1 вроде все работает, сразу объединила классы

--- pok/index.php
+++ pok/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<title> POKEMON GO </title>
         <meta content="text/html; charset=utf-8" http-equiv="content-type">
+
 		<link rel= "stylesheet" type="text/css" href="css/style.css">
 	</head>
 	
@@ -14,16 +15,12 @@
 		
 		<div class="up">
 		
-			<ul  class="menu">
-				<?php include 'menu.php'; //все тут плохо, знаю
-				echo Menu::f(0); 
-				echo Menu::f(1); 
-				echo Menu::f(2)?>
-				<li><a href="start.html">Start</a></li>
-				<li><a href="about.html">About</a></li>
-				<li><a href="top.html">Top List</a></li>
-			</ul>
+				<? include 'menu.php'; 
+				echo Menu::getMenu($_GET['page']); 
+				
+				?>
 		</div>
+		<? echo Menu::getPage($_GET['page']);?>
 	</div>
 		
 	</body>
--- pok/menu.php
+++ pok/menu.php
@@ -1,34 +1,49 @@
 <?php class Menu{
 	
-public $items = array(
+public static $items = array(
 
-array(
+1=>array(
 'title'=>'Start',
-'href'=>'start.php',
-'id'=>'1'
+'href'=>'index.php?page=1',
+'content'=>'assets/start.html'
 ),
 
-array(
+2=>array(
 'title'=>'About',
-'href'=>'about.php',
-'id'=>'2'
+'href'=>'index.php?page=2',
+'content'=>'assets/about.html'
 ),
 
-array(
+3=>array(
 'title'=>'Top List',
-'href'=>'top.php',
-'id'=>'3'
+'href'=>'index.php?page=3',
+'content'=>'assets/top.html'
 )
 );
 
-static function f($a)
+public  static function getPage($p)
+ {
+	if (array_key_exists ( $p , self::$items)==true){
+	 $page=file_get_contents(self::$items[$p]['content']);
+	}
+	 return $page;
+ }
+
+public static function getMenu($a)
 {
-	///все тут плохо, знаю
-	
-	$q="<li><a href=".$items[$a]['href'].">".$items[$a][title]."</a></li>";
-	
+	$q="<ul class=\"menu\">";
+	foreach(self::$items as $key => $value){
+		if ($a==$key)
+		{
+           $q.="<li><a href=".$value['href']." class=\"disabled\">".$value['title']."</a></li>";
+		}
+		else
+			$q.="<li><a href=".$value['href'].">".$value['title']."</a></li>";
+	}
+	$q.="</ul>";
 	return $q;
 }
 }
+
 
 ?>
